7829bae42aa9fd306ba8ebf31bab7ab33fb8b10d
Michael Bradford <esspecialmkb@gmail.com>
2019-09-23 17:50:19 -0400

    Regions display independent colors now
    But... Positions of the regions are not accurate...

--- JSDEV/scripts/mazeDev.js
+++ JSDEV/scripts/mazeDev.js
@@ -1,21 +1,90 @@
 var map = [];
 
+var cTime = 0;
+var dTime = 0;
+var lTime = 0;
+
+var regionList = [];
 var regionCount = 0;
 var newRegionId = function() {
 	regionCount += 1;
 	return regionCount;
 };
 
-// HSV to RGB conversion utility
-// from https://en.wikipedia.org/wiki/HSL_and_HSV#To_RGB
-convertHSV = funtion(hue, value, saturation) {
-	var chroma = value * saturation;
-	var huePrime = hue / 60;
-	var x = c * (1 - Math.abs(
+/**
+ * Converts an HSV color value to RGB. Conversion formula
+ * adapted from http://en.wikipedia.org/wiki/HSV_color_space.
+ * Assumes h, s, and v are contained in the set [0, 1] and
+ * returns r, g, and b in the set [0, 255].
+ * * Altered to convert h from (0, 360) range to (0, 1) *
+ *
+ * @param   Number  h       The hue
+ * @param   Number  s       The saturation
+ * @param   Number  v       The value
+ * @return  Array           The RGB representation
+ */
+hsvToRgb = function (h, s, v){
+    var r, g, b;
+
+    var i = Math.floor((h / 360) * 6);
+    var f = (h / 360) * 6 - i;
+    var p = v * (1 - s);
+    var q = v * (1 - f * s);
+    var t = v * (1 - (1 - f) * s);
+
+    switch(i % 6){
+        case 0: r = v, g = t, b = p; break;
+        case 1: r = q, g = v, b = p; break;
+        case 2: r = p, g = v, b = t; break;
+        case 3: r = p, g = q, b = v; break;
+        case 4: r = t, g = p, b = v; break;
+        case 5: r = v, g = p, b = q; break;
+    }
+
+    return [r * 255, g * 255, b * 255];
+}
+
+
+
+function Region(){
+	this.locX	= null;		// Room location
+	this.locY	= null;
+	this.dimX	= null;		// Room dimensions/size
+	this.dimY	= null;
+
+	this.id		= null;
+	this.type	= null;		// Room or path?
+
+	this.fillStyle	= null;
+}
+
+Region.prototype.init = function( x, y, sx, sy, id, type, fill ) {		
+	this.locX = x;
+	this.locY = y;
+	this.dimX = sx;
+	this.dimY = sy;
+
+	this.id = id;
+	this.type = type;
+		
+	rgb = hsvToRgb(Math.floor(Math.random() * 360), 1, 0.8);
+
+	this.fillStyle = "rgb(" +rgb[0]+ " ," + rgb[1] + " ," + rgb[2] + ")";
 };
 
-var regionList = [];
+Region.prototype.draw = function () {
+	ctx.fillStyle = this.fillStyle;	
+	ctx.strokeStyle = "black";	
 
+	for(var y = 0; y < this.dimY; ++y) {
+		for( var x = 0; x < this.dimX; ++x) {
+			ctx.fillRect( (x + this.locX) * tileSize, (y + this.locY) * tileSize, tileSize, tileSize);
+			ctx.strokeRect( (x + this.locX) * tileSize, (y + this.locY) * tileSize, tileSize, tileSize);
+		}
+	}
+};
+
+// /*
 var region = {
 	locX	: null,		// Room location
 	locY	: null,
@@ -37,11 +106,29 @@ var region = {
 
 		this.id = id;
 		this.type = type;
-		this.fillStyle = fill;
+		
+		var rgb = hsvToRgb(Math.floor(Math.random() * 360), 1, 0.8);
+
+		this.fillStyle = "rgb(" +rgb[0]+ " ," + rgb[1] + " ," + rgb[2] + ")";
 		
 		return obj;
+	},
+
+	draw	: function() {
+		ctx.fillStyle = this.fillStyle;	
+		ctx.strokeStyle = "black";	
+
+		for(var y = 0; y < this.dimY; ++y) {
+			for( var x = 0; x < this.dimX; ++x) {
+				ctx.fillRect( (x + this.locX) * tileSize, (y + this.locY) * tileSize, tileSize, tileSize);
+				ctx.strokeRect( (x + this.locX) * tileSize, (y + this.locY) * tileSize, tileSize, tileSize);
+			}
+		}
 	}
+	
 };
+
+//*/
 
 var mapH = 63;
 var mapW = 63;
@@ -122,20 +209,28 @@ genRoom = function(min, max) {
 		// Attempting to return here for some reason causes rooms to overlap
 		return false;
 	}else if(passing == 0) {
-		var newRegion = region.create( pX, pY, rX, rY, newRegionId(), 1, "green" );
+		// var newRegion = region.create( pX, pY, rX, rY, newRegionId(), 1, "green" );
+		var newRegion = new Region();
+		newRegion.init( pX, pY, rY, rY, newRegionId(), 1, "green");
+		regionList.push( newRegion );
+
 		for(var iX = 0; iX < rX; ++iX) {
 			for( var iY = 0; iY < rY; ++iY) {
 				map[ (( (iY + pY) * mapH) + (iX + pX) ) ] = 1;
 			}
 		}
-		regionList.push( newRegion );
+		
 		return true;
 	}
 };
 
-drawGame = function() {
+drawGame = function(time) {
+
+		
+
 	ctx.fillStyle = "black";
 	ctx.clearRect(0,0, 640, 640);
+
 
 	for(var y = 0; y < mapH; ++ y) {
 		for( var x = 0; x < mapW; ++x) {
@@ -144,6 +239,8 @@ drawGame = function() {
 				case 0:
 					ctx.fillStyle = "grey";
 					ctx.strokeStyle = "white";
+					ctx.fillRect( x * tileSize, y * tileSize, tileSize, tileSize);
+					ctx.strokeRect( x * tileSize, y * tileSize, tileSize, tileSize);
 					break;
 				case 1:
 					ctx.fillStyle = "green";
@@ -154,9 +251,15 @@ drawGame = function() {
 					ctx.strokeStyle = "black";
 			}
 
-			ctx.fillRect( x * tileSize, y * tileSize, tileSize, tileSize);
-			ctx.strokeRect( x * tileSize, y * tileSize, tileSize, tileSize);
+			//ctx.fillRect( x * tileSize, y * tileSize, tileSize, tileSize);
+			//ctx.strokeRect( x * tileSize, y * tileSize, tileSize, tileSize);
 		}
 	}
+
+	for(var r = 0; r < regionList.length; ++r) {
+		// For some reason, different regions are holding references to the same fillStyle, possibly the same location too
+		regionList[r].draw();
+	}	
+				 
 	requestAnimationFrame(drawGame);
 };
