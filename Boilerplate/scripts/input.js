6b5e60f85a6a431aa2544cd68d2811a17e077c0c
Michael Bradford <esspecialmkb@gmail.com>
2019-11-22 14:46:07 -0500

    Merged touch event handlers

--- Boilerplate/scripts/input.js
+++ Boilerplate/scripts/input.js
@@ -8,7 +8,6 @@ var enableKeyLogging = function(value){
 
 function keyDown(e) {
    	var x = e.which || e.keyCode;  // which or keyCode depends on browser support
-	abc
 	//e.preventDefault();
 	if(keyLogging == true){
 		console.log("Key Log: " + x + " down");
@@ -20,7 +19,7 @@ function keyUp(e) {
 
 	//e.preventDefault();
 	if(keyLogging){
-		console.log("Key Log: " + x + " down");
+		console.log("Key Log: " + x + " up");
 	}
 }
 
@@ -55,17 +54,20 @@ function mouseDown(e){
 	var button = e.button || e.which;
 	var x = e.clientX;
 	var y = e.clientY;
+	console.log("mouseDown " + e.clientX + ", " + e.clientY); 
 }
 
 function mouseUp(e){
 	var button = e.button || e.which;
 	var x = e.clientX;
 	var y = e.clientY;
+	console.log("mouseUp " + e.clientX + ", " + e.clientY);
 }
 
 function mouseMove(e){
 	var dX = e.movementX;
 	var dY = e.movementY;
+	console.log("mouseMove " + e.clientX + ", " + e.clientY);
 }
 
 var setMouseEventListener = function( func, type){
@@ -76,25 +78,127 @@ var removeMouseEventListener = function( func, type){
 	window.removeEventListener( type, func );
 }
 
-//window.addEventListener('onclick', mouseClick);	//The event occurs when the user clicks on an element
-//window.addEventListener('onmousedown', mouseDown);	//The event occurs when the user presses a mouse button over an element
-//window.addEventListener('onmouseup', mouseUp);	//The event occurs when a user releases a mouse button over an element
-//window.addEventListener('onmousemove", mouseMove);	//The event occurs when the pointer is moving while it is over an element
+window.addEventListener('onclick', mouseClick);	//The event occurs when the user clicks on an element
+window.addEventListener('onmousedown', mouseDown, false);	//The event occurs when the user presses a mouse button over an element
+window.addEventListener('onmouseup', mouseUp, false);	//The event occurs when a user releases a mouse button over an element
+window.addEventListener('onmousemove', mouseMove, true);	//The event occurs when the pointer is moving while it is over an element
 //window.addEventListener('oncontextmenu', mouseContext);//Right click
 
 //	Touch Input
 var touchLogging = false;
+var activeTouches = [];
+var touchObjL = null;
+var touchObjR = null;
+var startLX = 0;
+var startLY = 0;
+var startRX = cvs.width/2;
+var startRY = 0;
+var distLX = 0;
+var distLY = 0;
+var distRX = 0;
+var distRY = 0;
 
 function touchStart(e){
-	var touches = e.changedTouches;
+	var t = e.changedTouches;
+
+	switch(t.length){
+		case 1:
+			if( t[0].pageX <= cvs.width/2 ){
+				touchObjL = t[0];
+				startLX = t[0].pageX;
+				startLY = t[0].pageY;
+			}else{
+				touchObjR = t[0];
+				startRX = t[0].pageX;
+				startRY = t[0].pageY;
+			}
+			break;
+		case 2:
+			if( t[0].pageX <= cvs.width/2 ){
+				touchObjL = t[0];
+				startLX = t[0].pageX;
+				startLY = t[0].pageY;
+			}else{
+				touchObjR = t[1];
+				startRX = t[1].pageX;
+				startRY = t[1].pageY;
+			}
+			break;
+	}
 }
 
 function touchMove(e){
-	var touches = e.changedTouches;
+	var t = e.changedTouches;
+
+	switch(t.length){
+		case 1:
+			if( t[0].pageX <= cvs.width/2 ){
+				touchObjL = t[0];
+				distLX = startLX - t[0].pageX;
+				distLY = startLY - t[0].pageY;
+			}else{
+				touchObjR = t[0];
+				distRX = startRX - t[0].pageX;
+				distRY = startRY - t[0].pageY;
+			}
+			break;
+		case 2:
+			if( t[0].pageX <= cvs.width/2 ){
+				touchObjL = t[0];
+				touchObjR = t[1];
+				distLX = startLX - t[0].pageX;
+				distLY = startLY - t[0].pageY;
+
+				distRX = startRX - t[1].pageX;
+				distRY = startRY - t[1].pageY;
+			}else{
+				touchObjL = t[1];
+				touchObjR = t[0];
+				distLX = startLX - t[1].pageX;
+				distLY = startLY - t[1].pageY;
+
+				distRX = startRX - t[0].pageX;
+				distRY = startRY - t[0].pageY;
+			}
+			break;
+	}
 }
 
 function touchEnd(e){
-	var touches = e.changedTouches;
+	var t = e.changedTouches;
+
+	switch(t.length){
+		case 1:
+			if( t[0].pageX <= cvs.width/2 ){
+				touchObjL = t[0];
+				distLX = 0;
+				distLY = 0;
+			}else{
+				touchObjR = t[0];
+				distRX = 0;
+				distRY = 0;
+			}
+			break;
+		case 2:
+			if( t[0].pageX <= cvs.width/2 ){
+				touchObjL = t[0];
+				touchObjR = t[1];
+				distLX = 0
+				distLY = 0;
+
+				distRX = 0;
+				distRY = 0;
+			}else{
+				touchObjL = t[1];
+				touchObjR = t[0];
+				distLX = 0;
+				distLY = 0;
+
+				distRX = 0;
+				distRY = 0;
+			}
+			break;
+	}
 }
 
 var setTouchEventListener = function( func, type ){
@@ -104,3 +208,7 @@ var setTouchEventListener = function( func, type ){
 var removeTouchEventListener = function( func, type ){
 	window.removeEventListener( type, func);
 }
+
+window.addEventListener('touchstart', touchStart, false);
+window.addEventListener('touchmove', touchEnd, false);
+window.addEventListener('touchend', touchEnd, false);
