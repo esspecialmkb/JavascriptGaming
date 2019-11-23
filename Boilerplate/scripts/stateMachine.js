e65419948526eecbdc89a8a5e834c23a5b54f63f
Michael Bradford <esspecialmkb@gmail.com>
2019-11-23 15:43:10 -0500

    Add files via upload

---
+++ Boilerplate/scripts/stateMachine.js
@@ -0,0 +1,65 @@
+//	This file is preceded by...
+//input.js
+//canvasHandle.js
+//state.js
+
+//FSM-Core
+var fsm = (function () {
+	var _currentState = null;
+	var _stateList = [];
+	var _animationFrameId = null;
+	
+	//	Called when states need to pass info to fsm
+	function _stateCallback(message) {
+		
+	};
+	
+	function _setState(state) {
+		_currentState.onStop();
+		_currentState = state;
+		_currentState.onStart();
+	};
+	
+	function _init() {
+		_stateList.push( new StartState( _stateCallback ));
+		_stateList.push( new PlayState( _stateCallback ));
+		_setState(_stateList[0] );
+	};
+	
+	function _update() {
+		_currentState.onUpdate();
+	};
+
+	function _registerAnimationFrame(){
+		_animationFrameID = window.requestAnimationFrame(_update);
+	}
+
+	function _deregisterAnimationFrame(){
+		if(_animationFrameId){
+			window.cancelAnimationFrame(_animationFrameId);
+			_animationFrameId = null;
+		}
+	}
+	
+	return {
+		stateCallback: _stateCallback,
+		init: _init,
+		setState: _setState,
+		update: _update		
+	};
+}) ();
+
+fsm.init();
+
+// Init is called first, to prepare the state machine and set initial state
+// The update method gets called once per-frame, updating the current state
+// The FSM and active states can set their own listeners for input
+
+//setKeyboardEventListener(keyDown,'keydown');  //keydown, keyup
+//removeKeyboardEventListener(keyUp,'keyup');
+
+//setMouseEventListener(func, eventType);	//click, mousedown, mouseup, mouseomve
+//removeMouseEventListener(func, eventType);
+
+//setTouchEventListener(func, eventType);	//touchstart, touchmove, touchend
+//removeTouchEventListener(func, eventType);
