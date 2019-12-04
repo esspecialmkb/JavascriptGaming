6fbe1db3d935b4c4854cb51d4f2a00f96ff131ce
Michael Bradford <esspecialmkb@gmail.com>
2019-12-04 18:03:12 -0500

    Character Dev

--- Babylon/scripts/mesh.js
+++ Babylon/scripts/mesh.js
@@ -32,8 +32,9 @@ var pelvis = {
 var leg = {
 	wU: 1.5,
 	wL: 1,
-	h: 5,
-	d: 1
+	h: 4,
+	d: 1,
+	t: 1
 };
 
 var foot = {
@@ -59,25 +60,25 @@ function BuildCharacter( scene ) {
 	// Torso Verts
 	var posTor = [
 		// Build torso (16 verts min)
-		torso.w/2, pelvis.h, torso.d/2,
-		torso.w/2, pelvis.h + (torso.h/2) - (torso.t/2), torso.d/2,
-		-torso.w/2, pelvis.h+ (torso.h/2) - (torso.t/2), torso.d/2,
-		-torso.w/2, pelvis.h, torso.d/2,
+		torso.w/2, leg.h + pelvis.h, torso.d/2,
+		torso.w/2, leg.h  + pelvis.h + (torso.h/2) - (torso.t/2), torso.d/2,
+		-torso.w/2, leg.h  + pelvis.h+ (torso.h/2) - (torso.t/2), torso.d/2,
+		-torso.w/2, leg.h  + pelvis.h, torso.d/2,
 		
-		torso.w/2, pelvis.h + (torso.h/2) - (torso.t/2), torso.d/2,
-		torso.w/2, pelvis.h + (torso.h/2) + (torso.t/2), torso.d/2,
-		-torso.w/2, pelvis.h + (torso.h/2) + (torso.t/2), torso.d/2,
-		-torso.w/2, pelvis.h+ (torso.h/2) - (torso.t/2), torso.d/2,
+		torso.w/2, leg.h  + pelvis.h + (torso.h/2) - (torso.t/2), torso.d/2,
+		torso.w/2, leg.h  + pelvis.h + (torso.h/2) + (torso.t/2), torso.d/2,
+		-torso.w/2, leg.h  + pelvis.h + (torso.h/2) + (torso.t/2), torso.d/2,
+		-torso.w/2, leg.h  + pelvis.h + (torso.h/2) - (torso.t/2), torso.d/2,
 		
-		torso.w/2, pelvis.h, -torso.d/2,
-		torso.w/2, pelvis.h + (torso.h/2) - (torso.t/2), -torso.d/2,
-		-torso.w/2, pelvis.h+ (torso.h/2) - (torso.t/2), -torso.d/2,
-		-torso.w/2, pelvis.h, -torso.d/2,
+		torso.w/2, leg.h  + pelvis.h, -torso.d/2,
+		torso.w/2, leg.h  + pelvis.h + (torso.h/2) - (torso.t/2), -torso.d/2,
+		-torso.w/2, leg.h  + pelvis.h+ (torso.h/2) - (torso.t/2), -torso.d/2,
+		-torso.w/2, leg.h  + pelvis.h, -torso.d/2,
 		
-		torso.w/2, pelvis.h + (torso.h/2) - (torso.t/2), -torso.d/2,
-		torso.w/2, pelvis.h + (torso.h/2) + (torso.t/2), -torso.d/2,
-		-torso.w/2, pelvis.h + (torso.h/2) + (torso.t/2), -torso.d/2,
-		-torso.w/2, pelvis.h+ (torso.h/2) - (torso.t/2), -torso.d/2,
+		torso.w/2, leg.h  + pelvis.h + (torso.h/2) - (torso.t/2), -torso.d/2,
+		torso.w/2, leg.h  + pelvis.h + (torso.h/2) + (torso.t/2), -torso.d/2,
+		-torso.w/2, leg.h  + pelvis.h + (torso.h/2) + (torso.t/2), -torso.d/2,
+		-torso.w/2, leg.h  + pelvis.h + (torso.h/2) - (torso.t/2), -torso.d/2
 	];
 	
 	console.log("posTor: " + posTor ); 
@@ -112,7 +113,7 @@ function BuildCharacter( scene ) {
 		1, 4, 12,
 		1, 12, 9,
 		
-		4, 5, 12,
+		4, 5, 13,
 		4, 13, 12,
 		
 		3, 11, 10,	// Left faces
@@ -146,15 +147,15 @@ function BuildCharacter( scene ) {
 	// Pelvis Verts
 	var posPel = [
 		// Build pelvis (8 verts min)
-		pelvis.w/2, 0, pelvis.d/2,
-		pelvis.w/2, pelvis.h, pelvis.d/2,
-		-pelvis.w/2, pelvis.h, pelvis.d/2,
-		-pelvis.w/2, 0, pelvis.d/2,
+		pelvis.w/2, leg.h, pelvis.d/2,
+		pelvis.w/2,  leg.h + pelvis.h, pelvis.d/2,
+		-pelvis.w/2, leg.h + pelvis.h, pelvis.d/2,
+		-pelvis.w/2, leg.h, pelvis.d/2,
 		
-		pelvis.w/2, 0, -pelvis.d/2,
-		pelvis.w/2, pelvis.h, -pelvis.d/2,
-		-pelvis.w/2, pelvis.h, -pelvis.d/2,
-		-pelvis.w/2, 0, -pelvis.d/2
+		pelvis.w/2, leg.h, -pelvis.d/2,
+		pelvis.w/2, leg.h + pelvis.h, -pelvis.d/2,
+		-pelvis.w/2, leg.h + pelvis.h, -pelvis.d/2,
+		-pelvis.w/2, leg.h, -pelvis.d/2
 	];
 	
 	console.log("posPel: " + posPel );
@@ -168,8 +169,8 @@ function BuildCharacter( scene ) {
 		1, 6, 5,	// top
 		1, 2, 6,
 		
-		7, 5, 4,	// back
-		7, 6, 5,
+		4, 5, 6,	// back
+		4, 6, 7,
 		
 		0, 4, 7,	// bottom
 		0, 7, 3,
@@ -192,11 +193,220 @@ function BuildCharacter( scene ) {
 		1, 0, 0, 1
 	];
 	
+	// Right Leg Verts (18 Vets)
+	var posLegR = [
+		leg.wU, 0, leg.d/2,		
+		leg.wU, (leg.h/2) - (leg.t/2), leg.d/2,
+		leg.wU - leg.wL, (leg.h/2) - (leg.t/2), leg.d/2,
+		leg.wU - leg.wL, 0, leg.d/2,
+		
+		leg.wU, leg.h/2, leg.d/2,
+		leg.wU - leg.wL, leg.h/2, leg.d/2,
+		
+		leg.wU, (leg.h/2) + (leg.t/2), leg.d/2,
+		leg.wU, leg.h, leg.d/2,
+		leg.wU - leg.wL, leg.h, leg.d/2,
+		leg.wU - leg.wL, (leg.h/2) + (leg.t/2), leg.d/2,
+		
+		leg.wU, 0, -leg.d/2,		
+		leg.wU, (leg.h/2) - (leg.t/2), -leg.d/2,
+		leg.wU - leg.wL, (leg.h/2) - (leg.t/2), -leg.d/2,
+		leg.wU - leg.wL, 0, -leg.d/2,
+		
+		leg.wU, (leg.h/2), -leg.d/2,
+		leg.wU, leg.h, -leg.d/2,
+		leg.wU - leg.wL, leg.h, -leg.d/2,
+		leg.wU - leg.wL, (leg.h/2), -leg.d/2
+	];
+	
+	var indLegR = [
+		0, 3, 2,	//Front faces
+		0, 2, 1,
+		
+		1, 2, 5,
+		1, 5, 4,
+		
+		4, 5, 9,
+		4, 9, 6,
+		
+		6, 9, 8,
+		6, 8, 7,
+		
+		// Top faces
+		7, 8, 16,
+		7, 16, 15,
+		
+		// Back faces
+		10, 11, 12,
+		10, 12, 13,
+		
+		11, 14, 17,
+		11, 17, 12,
+		
+		14, 15, 16,
+		14, 16, 17,
+		
+		// Bottom faces
+		0, 10, 13,
+		0, 13, 3,
+		
+		// Left faces
+		3, 13, 12,
+		3, 12, 2,
+		
+		2, 12, 17,
+		2, 17, 5,
+		
+		5, 17, 9, // Corner
+		
+		9, 17, 16,
+		9, 16, 8,
+		
+		// Right faces
+		10, 0, 1,
+		10, 1, 11,
+		
+		11, 1, 4,
+		11, 4, 14,
+		
+		14, 4, 6,
+		
+		6, 7, 15,
+		6, 15, 14
+	];
+	
+	var colLegR = [
+		0,0,1,1,
+		0,0,1,1,
+		0,0,1,1,
+		0,0,1,1,
+		0,0,1,1,
+		0,0,1,1,
+		0,0,1,1,
+		0,0,1,1,
+		0,0,1,1,
+		0,0,1,1,
+		0,0,1,1,
+		0,0,1,1,
+		0,0,1,1,
+		0,0,1,1,
+		0,0,1,1,
+		0,0,1,1,
+		0,0,1,1,
+		0,0,1,1
+	];
+	
+	// Left Leg Verts (18 Vets)
+	var posLegL = [
+		leg.wU, 0, leg.d/2,		
+		leg.wU, (leg.h/2) - (leg.t/2), leg.d/2,
+		leg.wU - leg.wL, (leg.h/2) - (leg.t/2), leg.d/2,
+		leg.wU - leg.wL, 0, leg.d/2,
+		
+		leg.wU, leg.h/2, leg.d/2,
+		leg.wU - leg.wL, leg.h/2, leg.d/2,
+		
+		leg.wU, (leg.h/2) + (leg.t/2), leg.d/2,
+		leg.wU, leg.h, leg.d/2,
+		leg.wU - leg.wL, leg.h, leg.d/2,
+		leg.wU - leg.wL, (leg.h/2) + (leg.t/2), leg.d/2,
+		
+		leg.wU, 0, -leg.d/2,		
+		leg.wU, (leg.h/2) - (leg.t/2), -leg.d/2,
+		leg.wU - leg.wL, (leg.h/2) - (leg.t/2), -leg.d/2,
+		leg.wU - leg.wL, 0, -leg.d/2,
+		
+		leg.wU, (leg.h/2), -leg.d/2,
+		leg.wU, leg.h, -leg.d/2,
+		leg.wU - leg.wL, leg.h, -leg.d/2,
+		leg.wU - leg.wL, (leg.h/2), -leg.d/2
+	];
+	
+	var indLegL = [
+		0, 3, 2,	//Front faces
+		0, 2, 1,
+		
+		1, 2, 5,
+		1, 5, 4,
+		
+		4, 5, 9,
+		4, 9, 6,
+		
+		6, 9, 8,
+		6, 8, 7,
+		
+		// Top faces
+		7, 8, 16,
+		7, 16, 15,
+		
+		// Back faces
+		10, 11, 12,
+		10, 12, 13,
+		
+		11, 14, 17,
+		11, 17, 12,
+		
+		14, 15, 16,
+		14, 16, 17,
+		
+		// Bottom faces
+		0, 10, 13,
+		0, 13, 3,
+		
+		// Left faces
+		3, 13, 12,
+		3, 12, 2,
+		
+		2, 12, 17,
+		2, 17, 5,
+		
+		5, 17, 9, // Corner
+		
+		9, 17, 16,
+		9, 16, 8,
+		
+		// Right faces
+		10, 0, 1,
+		10, 1, 11,
+		
+		11, 1, 4,
+		11, 4, 14,
+		
+		14, 4, 6,
+		
+		6, 7, 15,
+		6, 15, 14
+	];
+	
+	var colLegL = [
+		0,0,1,1,
+		0,0,1,1,
+		0,0,1,1,
+		0,0,1,1,
+		0,0,1,1,
+		0,0,1,1,
+		0,0,1,1,
+		0,0,1,1,
+		0,0,1,1,
+		0,0,1,1,
+		0,0,1,1,
+		0,0,1,1,
+		0,0,1,1,
+		0,0,1,1,
+		0,0,1,1,
+		0,0,1,1,
+		0,0,1,1,
+		0,0,1,1
+	];
+	
 	var positions = ConcatData( posPel, posTor );
+	positions = ConcatData( positions, posLegR );
 	//console.log("positions: " + positions );
 	var indices = ConcatIndices( indPel, indTor, posPel.length/3 );
+	indices = ConcatIndices( indices, indLegR, (posPel.length/3) + (posTor.length/3));
 	console.log("indices: " + indices );
 	var colors = ConcatData( colPel, colTor );
+	colors = ConcatData( colors, colLegR );
 	var normals = [];
 	
 	var vertData = new BABYLON.VertexData();
